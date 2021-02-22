ae05680eeb29a266426005a6f55084ab09ce31e6
menachem margolis <margolismendel5728@gmail.com>
2021-02-22 14:00:26 -0500

    Done.

--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,11 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export {username, city, image}
+// In the src/data/user.js file, there are three variables that have information about the owner of the portfolio site. Export all three variables using the named export syntax.
+
+// After you've exported them, import them in the following files:
+
+// Import the username and city variables in the src/components/Home.js file
+// Import the image variable in the src/components/About.js file
